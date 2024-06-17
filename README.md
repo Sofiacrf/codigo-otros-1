8853bed90ed8d7bd2b7385b928c394a5751457fb
sofiacrf <sofia.rojasflo@gmail.com>
2024-06-17 12:11:05 -0600

    Se solucionaron los problemas y agrego ReadMe

---
+++ README.md
@@ -0,0 +1,11 @@
+# LECTURA DE USUARIOS
+
+Se muestra una página que permite recibir el nombre de usuario, blog y ubicación. 
+
+## Instalación
+
+* Clonar el repositorio
+* Abrir el archivo en el servidor
+
+## Contribución 
+Si tienes alguna recomendación de mejoras, por favor contacta a los creadores.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json();
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
